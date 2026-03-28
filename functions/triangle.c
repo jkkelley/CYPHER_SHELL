aac235d4e3d9821b95182774ca479c3171a3696a
James Kelley <jkkelley86@gmail.com>
2026-03-27 20:53:38 -0500

    Working with Functions in C, getting the area of a triangle

---
+++ functions/triangle.c
@@ -0,0 +1,30 @@
+/* triangle.c */
+#include <stdio.h>
+
+/* Get the area of a triangle */
+int area_triangle(int base, int height)
+{
+    int area;
+
+    area = (base * height) / 2;
+
+    return area;
+}
+
+int main()
+{
+    int base, height, area;
+
+    printf("Base: ");
+    fflush(stdout);
+    scanf("%d", &base);
+
+    printf("Height: ");
+    fflush(stdout);
+    scanf("%d", &height);
+
+    area = area_triangle(base, height);
+    printf("The area is %d\n", area);
+
+    return 0;
+}
