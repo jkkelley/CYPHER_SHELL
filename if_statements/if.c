ae0ebe52e956b837522a33cfeab2583a279f77d1
James Kelley <jkkelley86@gmail.com>
2026-03-27 20:08:50 -0500

    If you know, you know -Biggie

--- if_statements/if.c
+++ if_statements/if.c
@@ -3,16 +3,38 @@
 
 int main()
 {
-    int x;
+    // int x;
 
-    x = 5;
+    // x = 5;
     
-    // Only happens if the condition is met
-    // In this case, x must equal 5
-    if (x == 5)
+    // // Only happens if the condition is met
+    // // In this case, x must equal 5
+    // if (x == 5)
+    // {
+    //     printf("It is %d\n", x);
+    // }
+
+    int velocity;
+    printf("How fast are you in km/h?\n");
+    scanf("%d", &velocity);
+
+    if (velocity > 100)
     {
-        printf("It is %d\n", x);
+        printf("You are driving too fast!\n");
     }
+    
+    else if (velocity > 80)
+    {
+        printf("You are driving safely\n");
+    }
+    // Catch all statement if previous conditions
+    // are not met
+    else
+    {
+        printf("Ok\n");
+    }
+
+    printf("\n\nYou've reached the end of the line\n\n");
 
     return 0;
 }
